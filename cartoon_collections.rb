f722c69961b57f842280585aae5c5ed477c0613e
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-28 19:29:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,10 @@
 def roll_call_dwarves# code an argument here
-  dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+index = 0
   
 end
 
 def summon_captain_planet# code an argument here
- planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
 end
 
 def long_planeteer_calls# code an argument here
@@ -13,5 +13,5 @@ end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
