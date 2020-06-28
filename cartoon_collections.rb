7570d870bb46343ed6e6cf7e6c8e920f259b45a7
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-28 19:27:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves# code an argument here
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+ planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 end
 
 def long_planeteer_calls# code an argument here
