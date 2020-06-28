f014de9c59d091a702e7144a735129fb18fdc20c
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-28 19:39:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,9 @@ def long_planeteer_calls(array)  # code an argument here
   
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
-  
+  array.String
+end
+string.first
 end
